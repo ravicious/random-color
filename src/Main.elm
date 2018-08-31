c23400c02c6960ce3d475dd0f3ce7a7f8e3edf78
Rafał Cieślak <ravicious@gmail.com>
2018-08-31 18:54:58 +0200

    Add the initial version of the page

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,87 @@
 module Main exposing (..)
 
-import Html exposing (Html, button, div, text)
+import Random
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Color exposing (Color)
+import Debug
 
 
 main =
-    text "Hello, World!"
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = (\_ -> Sub.none)
+        }
+
+
+colorToCssRgb : Color -> String
+colorToCssRgb color =
+    let
+        { red, green, blue } =
+            Color.toRgb color
+    in
+        ("rgb("
+            ++ (String.fromInt red)
+            ++ ", "
+            ++ (String.fromInt green)
+            ++ ", "
+            ++ (String.fromInt blue)
+            ++ ")"
+        )
+
+
+type alias Model =
+    Maybe Color
+
+
+byteGenerator : Random.Generator Int
+byteGenerator =
+    Random.int 0 255
+
+
+colorGenerator : Random.Generator Color
+colorGenerator =
+    Random.map3 Color.rgb byteGenerator byteGenerator byteGenerator
+
+
+init : () -> ( Model, Cmd Msg )
+init flags =
+    ( Nothing, Random.generate RandomColor colorGenerator )
+
+
+type Msg
+    = RandomColor Color
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        RandomColor color ->
+            ( Just color, Cmd.none )
+
+
+view model =
+    case model of
+        Nothing ->
+            text ""
+
+        Just color ->
+            div
+                [ style "height" "100vh"
+                , style "width" "100vw"
+                , style "background-color" (colorToCssRgb color)
+                , style "color" (color |> Color.complement |> colorToCssRgb)
+                , style "display" "flex"
+                , style "justify-content" "center"
+                , style "align-items" "center"
+                , style "font-family" "monospace"
+                , style "font-size" "calc(.75rem + 5.5vw)"
+                , style "text-align" "center"
+                ]
+                [ text (colorToCssRgb color)
+                , br [] []
+                , text ("#" ++ (Color.toHex color))
+                ]
