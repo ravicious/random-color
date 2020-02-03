8cd244630204857536963fa2d685904da523bd22
Rafał Cieślak <ravicious@gmail.com>
2020-02-03 21:25:12 +0100

    Update dependencies

--- src/Main.elm
+++ src/Main.elm
@@ -10,7 +10,7 @@ import Html.Events exposing (..)
 import Random
 import Route
 import Svg exposing (path, svg)
-import Svg.Attributes exposing (d, fill, height, viewBox, width)
+import Svg.Attributes exposing (d, fill, viewBox, width)
 import Url exposing (Url)
 import Url.Parser as Parser exposing ((</>), oneOf, s)
 
@@ -322,7 +322,7 @@ colorToCssRgb color =
 octocat : Html msg
 octocat =
     a [ href "https://github.com/ravicious/random-color", class "github-corner", attribute "aria-label" "View source on Github" ]
-        [ svg [ width "80", height "80", viewBox "0 0 250 250", Svg.Attributes.style "fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" ]
+        [ svg [ Svg.Attributes.width "80", Svg.Attributes.height "80", viewBox "0 0 250 250", Svg.Attributes.style "fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" ]
             [ path [ d "M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" ]
                 []
             , path [ d "M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2", fill "currentColor", Svg.Attributes.style "transform-origin: 130px 106px;", Svg.Attributes.class "octo-arm" ]
